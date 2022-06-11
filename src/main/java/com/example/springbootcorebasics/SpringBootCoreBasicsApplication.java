30b6be9ef363bf696f895a37eece490804e850dd
Techxtor <dev.techxtor@gmail.com>
2022-06-11 23:55:44 +0530

    Add Missing Files

---
+++ src/main/java/com/example/springbootcorebasics/SpringBootCoreBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootcorebasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCoreBasicsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCoreBasicsApplication.class, args);
+    }
+
+}
